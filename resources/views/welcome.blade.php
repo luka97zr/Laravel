3f9b2cf8049147853e4dde1f49368247ba232d41
Luka Trkulja <luka97zr@gmail.com>
2022-04-10 21:40:44 +0200

    Add initial files to repository with some articles

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <article>
+        <h1>Article first</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quas repudiandae deleniti itaque optio soluta dignissimos possimus earum dolorum perspiciatis, excepturi animi nobis dolores minima blanditiis facere distinctio accusantium nemo.</p>
+    </article>
+    <article>
+        <h1>Article second</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quas repudiandae deleniti itaque optio soluta dignissimos possimus earum dolorum perspiciatis, excepturi animi nobis dolores minima blanditiis facere distinctio accusantium nemo.</p>
+    </article>
+    <article>
+        <h1>Article third</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quas repudiandae deleniti itaque optio soluta dignissimos possimus earum dolorum perspiciatis, excepturi animi nobis dolores minima blanditiis facere distinctio accusantium nemo.</p>
+    </article>
+</body>
+</html>
