3b5199cbd5f807e4aefd3943d3c7d7e68ef5f3f4
Luka Trkulja <luka97zr@gmail.com>
2022-04-13 00:34:27 +0200

    Add each loop to get all the post from files, create Post object

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,17 +8,26 @@
     <title>Document</title>
 </head>
 <body>
-    <article>
-        <h1>Article first</h1>
+    <!-- <article>
+        <h1><a href="post/my-first-post">Article first</a></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quas repudiandae deleniti itaque optio soluta dignissimos possimus earum dolorum perspiciatis, excepturi animi nobis dolores minima blanditiis facere distinctio accusantium nemo.</p>
     </article>
     <article>
-        <h1>Article second</h1>
+        <h1><a href="post/my-second-post">Article second</a></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quas repudiandae deleniti itaque optio soluta dignissimos possimus earum dolorum perspiciatis, excepturi animi nobis dolores minima blanditiis facere distinctio accusantium nemo.</p>
     </article>
     <article>
-        <h1>Article third</h1>
+        <h1><a href="post/my-third-post">Article third</a></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A quas repudiandae deleniti itaque optio soluta dignissimos possimus earum dolorum perspiciatis, excepturi animi nobis dolores minima blanditiis facere distinctio accusantium nemo.</p>
-    </article>
+    </article> -->
+    <?php 
+
+        foreach($posts as $post) : ?>
+            <article>
+                <?= $post ?>
+            </article>
+        <?php
+        endforeach;
+    ?>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use League\CommonMark\Extension\FrontMatter\Data\LibYamlFrontMatterParser;
+use League\CommonMark\Extension\FrontMatter\Data\SymfonyYamlFrontMatterParser;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Translation\Dumper\YamlFileDumper;
+use Symfony\Component\VarDumper\VarDumper;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +20,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('posts',[
+    //     'posts' => Post::all()
+    // ]);
 });
+
+Route::get('post/{post}', function ($slug) {
+
+    $post = Post::find($slug);
+
+    return view('post',[
+        'post' => $post
+    ]);
+
+})->where('post','[A-z_\-]+');
