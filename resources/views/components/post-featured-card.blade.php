0d9037e7099bdac454a871b6f5e223e8b2ab2df4
Luka Trkulja <luka.trkulja@vegait.rs>
2022-05-02 19:36:29 +0200

    Post factory changes, posts blade component refactor

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -25,7 +25,7 @@
 
             <div class="text-sm mt-2">
                 <p>
-                    {{$post->excerpt}}
+                    {!!$post->excerpt!!}
                 </p>
 
             </div>
