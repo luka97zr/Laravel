67e6c587fda2cce9c5dbdc0bdd3566919c4d50c3
Luka Trkulja <luka97zr@gmail.com>
2022-05-08 22:46:52 +0200

    Merge category and search results by creating changes in href of a dropdown and changing SQL query

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         Post::truncate();
         Category::truncate();
 
-        User::factory()->create();
+        User::factory(3)->create();
 
         Post::factory(10)->create();
     //    $user = User::factory()->create();
