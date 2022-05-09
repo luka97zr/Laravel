ebd2c815956772a97243267953f38bd6fdd2ccc1
Luka Trkulja <luka.trkulja@vegait.rs>
2022-05-09 16:21:19 +0200

    Show success flash msg

--- public/main.js
+++ public/main.js
@@ -1,6 +1,11 @@
 const categoryBtn = document.querySelector('.category__btn');
 const categoryMenu = document.querySelector('.category__menu');
+const registerMsg = document.querySelector('.js-register-msg');
 
 categoryBtn.addEventListener('click',()=>{
     categoryMenu.classList.toggle('active')
 })
+
+if(registerMsg) setTimeout(() => {
+    registerMsg.remove()
+}, 2500);
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -57,5 +57,10 @@
             </div>
         </footer>
     </section>
+    @if (session()->has('success'))
+        <div class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm js-register-msg">
+            <p>{{session('success')}}</p>
+        </div>
+    @endif
 </body>
 </html>
